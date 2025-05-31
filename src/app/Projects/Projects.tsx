1aae5bdb5aa063414df23789e09404ea09eff084
Monoe <148734398+LonelyFort@users.noreply.github.com>
2025-05-31 17:51:19 +0800

    Add mobile breakpoint design

--- src/app/Projects/Projects.tsx
+++ src/app/Projects/Projects.tsx
@@ -7,70 +7,121 @@ import TypeScript from "../../../resources/images/svg/typescript-icon.svg";
 import Python from "../../../resources/images/svg/python-icon.svg";
 
 import Blender from "../../../resources/images/svg/blender-icon.svg";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function Projects() {
   const techStack = [React, TypeScript, Python];
 
   return (
-    <div className="w-full flex flex-grow gap-20 h-fit mt-10 ml-10 mr-10 place-content-center">
-      <div className="flex flex-col border border-white p-6 rounded-xl min-w-[500px] max-w-[650px] flex-grow">
-        <div className="w-full p-4 text-[40px] font-bold text-center">
-          Tech Projects
+    <>
+      {/*Desktop*/}
+      <div className="xl:flex hidden w-full flex-grow gap-20 h-fit mt-10 ml-10 mr-10 place-content-center">
+        <div className="flex flex-col border border-white p-6 rounded-xl min-w-[500px] max-w-[650px] flex-grow">
+          <div className="w-full p-4 text-[40px] font-bold text-center">
+            Tech Projects
+          </div>
+          <div className="w-full h-[400px] bg-gray-400 relative rounded-xl">
+            <Image
+              src={Project}
+              alt=""
+              fill={true}
+              className="rounded-xl border-3 border-white"
+            />
+          </div>
+          <div className="py-4 flex-1 overflow-y-auto">
+            <h2 className="text-[30px] font-bold">
+              Email Confirmation Telegram Bot
+            </h2>
+            <br />
+            <p className="text-[25px]">
+              This application intends to mimic the behaviour of sending
+              confirmation email to students after they have entered their
+              school email address into the input bar, but deployed onto
+              Telegram Mini App via a Telegram Bot.
+            </p>
+            <br />
+            <div className="flex gap-8 py-2">
+              {techStack.map((item, index) => (
+                <div key={index} className="bg-[#6e6e6e] rounded-[22px] p-2">
+                  <Image src={item} className="size-15" alt="" />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="w-full h-[400px] bg-gray-400 relative rounded-xl">
-          <Image
-            src={Project}
-            alt=""
-            fill={true}
-            className="rounded-xl border-3 border-white"
-          />
-        </div>
-        <div className="py-4 flex-1 overflow-y-auto">
-          <h2 className="text-[30px] font-bold">
-            Email Confirmation Telegram Bot
-          </h2>
-          <br />
-          <p className="text-[25px]">
-            This application intends to mimic the behaviour of sending
-            confirmation email to students after they have entered their school
-            email address into the input bar, but deployed onto Telegram Mini
-            App via a Telegram Bot.
-          </p>
-          <br />
-          <div className="flex gap-8 py-2">
-            {techStack.map((item, index) => (
-              <div key={index} className="bg-[#6e6e6e] rounded-[22px] p-2">
-                <Image src={item} className="size-15" alt="" />
-              </div>
-            ))}
+        <div className="flex flex-col p-6 border border-white rounded-xl min-w-[500px] max-w-[650px] flex-grow">
+          <div className="w-full p-4 text-[40px] font-bold text-center">
+            Graphic Design Projects
+          </div>
+          <div className="w-full h-[400px] bg-gray-400 relative rounded-xl">
+            <Image
+              src={Donut}
+              alt=""
+              fill={true}
+              className="rounded-xl border-1 border-white"
+            />
+          </div>
+          <div className="py-4 flex-1 overflow-y-auto">
+            <h2 className="text-[30px] font-bold">
+              Donut Project (In Progress)
+            </h2>
+            <br />
+            <p className="text-[25px]">
+              Part of Blender tutorial by Blender Guru. Model mostly finished.
+              Project at pause until I get a better GPU for rendering.
+            </p>
+            <br />
+            <div className="bg-[#6e6e6e] rounded-[22px] p-2 w-fit">
+              <Image src={Blender} className="size-15" alt="" />
+            </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col p-6 border border-white rounded-xl min-w-[500px] max-w-[650px] flex-grow">
-        <div className="w-full p-4 text-[40px] font-bold text-center">
-          Graphic Design Projects
-        </div>
-        <div className="w-full h-[400px] bg-gray-400 relative rounded-xl">
-          <Image
-            src={Donut}
-            alt=""
-            fill={true}
-            className="rounded-xl border-1 border-white"
-          />
-        </div>
-        <div className="py-4 flex-1 overflow-y-auto">
-          <h2 className="text-[30px] font-bold">Donut Project (In Progress)</h2>
-          <br />
-          <p className="text-[25px]">
-            Part of Blender tutorial by Blender Guru. Model mostly finished.
-            Project at pause until I get a better GPU for rendering.
-          </p>
-          <br />
-          <div className="bg-[#6e6e6e] rounded-[22px] p-2 w-fit">
-            <Image src={Blender} className="size-15" alt="" />
-          </div>
-        </div>
+
+      {/*Mobile*/}
+      <div className="xl:hidden">
+        <h1 className="font-crimson text-[40px] text-center py-4">Projects</h1>
+        <Tabs defaultValue="tech" className="items-center">
+          <TabsList>
+            <TabsTrigger value="tech" className="w-[165px]">
+              <h1 className="text-[16px] text-center">Tech Projects</h1>
+            </TabsTrigger>
+            <TabsTrigger value="graphic" className="w-[165px]">
+              <h1 className="text-[12px] text-center">
+                Graphic Design Projects
+              </h1>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="tech">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem
+                value="item-1"
+                className="border rounded-xl border-white p-4"
+              >
+                <AccordionTrigger>Test</AccordionTrigger>
+                <AccordionContent></AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </TabsContent>
+          <TabsContent value="graphic">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem
+                value="item-1"
+                className="border rounded-xl border-white p-4"
+              >
+                <AccordionTrigger>Test</AccordionTrigger>
+                <AccordionContent></AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </TabsContent>
+        </Tabs>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/components/MobileSideMenu/MobileSideMenu.tsx
+++ src/app/components/MobileSideMenu/MobileSideMenu.tsx
@@ -1,5 +1,6 @@
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -35,30 +36,41 @@ const MobileSideMenu: React.FC<MobileSideMenuProps> = ({
           <SheetDescription />
         </SheetHeader>
         <div className="grid gap-8 px-8">
-          <button
-            className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
-            onClick={() => handleButtonClick("about")}
-          >
-            <h1 className="text-[30px]">About</h1>
-          </button>
-          <button
-            className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
-            onClick={() => handleButtonClick("experience")}
-          >
-            <h1 className="text-[30px]">Experience</h1>
-          </button>
-          <button
-            className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
-            onClick={() => handleButtonClick("projects")}
-          >
-            <h1 className="text-[30px]">Projects</h1>
-          </button>
-          <button
-            className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
-            onClick={() => handleButtonClick("contacts")}
-          >
-            <h1 className="text-[30px]">Contacts</h1>
-          </button>
+          <SheetClose asChild>
+            <button
+              className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
+              onClick={() => handleButtonClick("about")}
+            >
+              <h1 className="text-[30px]">About</h1>
+            </button>
+          </SheetClose>
+
+          <SheetClose asChild>
+            <button
+              className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
+              onClick={() => handleButtonClick("experience")}
+            >
+              <h1 className="text-[30px]">Experience</h1>
+            </button>
+          </SheetClose>
+
+          <SheetClose asChild>
+            <button
+              className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
+              onClick={() => handleButtonClick("projects")}
+            >
+              <h1 className="text-[30px]">Projects</h1>
+            </button>
+          </SheetClose>
+
+          <SheetClose asChild>
+            <button
+              className="hover:bg-[#474747] rounded-[40px] focus:outline-none py-2"
+              onClick={() => handleButtonClick("contacts")}
+            >
+              <h1 className="text-[30px]">Contacts</h1>
+            </button>
+          </SheetClose>
         </div>
       </SheetContent>
     </Sheet>
